5d5f2d5e7ca25313fc967e6cd8aeefa251a8089e
Deepakab17 <deepaknab17@gmail.com>
2025-12-02 23:54:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function bodyload(){
+    window.document.images[0].src="./images/922028.jpg"
+    window.document.forms[0].elements[1].value="Register"
+    window.document.forms[1].elements[1].value="Login"
+};
